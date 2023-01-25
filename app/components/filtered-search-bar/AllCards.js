c70dde7af8614a6bb6763bfd2ee9399365392ce8
Palanikannan1437 <palanikannan.m2020@vitstudent.ac.in>
2023-01-25 21:23:32 +0530

    fixed devops path not showing up bug

--- app/components/filtered-search-bar/AllCards.js
+++ app/components/filtered-search-bar/AllCards.js
@@ -7,7 +7,7 @@ function AllCards({ data, search }) {
   return (
     <motion.div layout className="grid place-items-center grid-cols-1 gap-10 sm:grid-cols-2 xl:grid-cols-3 md:mt-10 md:pb-10">
       {search(data)?.map((x) => {
-        if (x.id > 7) {
+        if (x.id >= 7) {
           return <Card name={x.name} info={x.info} key={x.id} value={x.value} type={x.genre} />;
         }
       })}
